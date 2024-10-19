5d7fb23b65875a15ba9b904c2f0165de954fcbe5
kraschowetz <joao.kraschowetz@gmail.com>
2024-10-18 21:56:43 -0300

    gambiarra do git

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kraschowetz/Documents/C++/shader_sample/src/core/engine.cpp" "CMakeFiles/main.dir/src/core/engine.cpp.o" "gcc" "CMakeFiles/main.dir/src/core/engine.cpp.o.d"
+  "/home/kraschowetz/Documents/C++/shader_sample/src/core/timer.cpp" "CMakeFiles/main.dir/src/core/timer.cpp.o" "gcc" "CMakeFiles/main.dir/src/core/timer.cpp.o.d"
+  "/home/kraschowetz/Documents/C++/shader_sample/src/gfx/canvas.cpp" "CMakeFiles/main.dir/src/gfx/canvas.cpp.o" "gcc" "CMakeFiles/main.dir/src/gfx/canvas.cpp.o.d"
+  "/home/kraschowetz/Documents/C++/shader_sample/src/gfx/renderer.cpp" "CMakeFiles/main.dir/src/gfx/renderer.cpp.o" "gcc" "CMakeFiles/main.dir/src/gfx/renderer.cpp.o.d"
+  "/home/kraschowetz/Documents/C++/shader_sample/src/gfx/vao.cpp" "CMakeFiles/main.dir/src/gfx/vao.cpp.o" "gcc" "CMakeFiles/main.dir/src/gfx/vao.cpp.o.d"
+  "/home/kraschowetz/Documents/C++/shader_sample/src/gfx/vbo.cpp" "CMakeFiles/main.dir/src/gfx/vbo.cpp.o" "gcc" "CMakeFiles/main.dir/src/gfx/vbo.cpp.o.d"
+  "/home/kraschowetz/Documents/C++/shader_sample/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/core/engine.cpp.o"
+  "CMakeFiles/main.dir/src/core/engine.cpp.o.d"
+  "CMakeFiles/main.dir/src/core/timer.cpp.o"
+  "CMakeFiles/main.dir/src/core/timer.cpp.o.d"
+  "CMakeFiles/main.dir/src/gfx/canvas.cpp.o"
+  "CMakeFiles/main.dir/src/gfx/canvas.cpp.o.d"
+  "CMakeFiles/main.dir/src/gfx/renderer.cpp.o"
+  "CMakeFiles/main.dir/src/gfx/renderer.cpp.o.d"
+  "CMakeFiles/main.dir/src/gfx/vao.cpp.o"
+  "CMakeFiles/main.dir/src/gfx/vao.cpp.o.d"
+  "CMakeFiles/main.dir/src/gfx/vbo.cpp.o"
+  "CMakeFiles/main.dir/src/gfx/vbo.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
